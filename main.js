2c6d6881a1e3a565f219d8dfbf8b1a27fb49e020
Alex Wendland <me@alexwendland.com>
2014-10-25 11:45:39 -0700

    Improved UI and added remote json loading

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 angular.module('jsonTreeDemo', ['angular-json-tree'])
-	.controller('DemoCtrl', ['$scope', function ($scope) {
-		$scope.jsonString = "{\"glossary\":{\"title\":\"example glossary\",\"GlossDiv\":{\"title\":\"S\",\"GlossList\":{\"GlossEntry\":{\"ID\":\"SGML\",\"SortAs\":\"SGML\",\"GlossTerm\":\"Standard Generalized Markup Language\",\"Acronym\":\"SGML\",\"Abbrev\":\"ISO 8879:1986\",\"GlossDef\":{\"para\":\"A meta-markup language, used to create markup languages such as DocBook.\",\"GlossSeeAlso\":[\"GML\",\"XML\"]},\"GlossSee\":\"markup\"}}}}}";
-		$scope.getJson = function () {
-			if (!$scope.jsonString) {
-				return {};
-			} else {
-				return JSON.parse($scope.jsonString);
+	.controller('DemoCtrl', ['$scope', '$http', function ($scope, $http) {
+		$scope.jsonString = "{\n  \"id\": \"0001\",\n  \"type\": \"donut\",\n  \"name\": \"Cake\",\n  \"ppu\": 0.55,\n  \"batters\":\n    {\n      \"batter\":\n        [\n          { \"id\": \"1001\", \"type\": \"Regular\" },\n          { \"id\": \"1002\", \"type\": \"Chocolate\" },\n          { \"id\": \"1003\", \"type\": \"Blueberry\" },\n          { \"id\": \"1004\", \"type\": \"Devil's Food\" }\n        ]\n    },\n  \"topping\":\n    [\n      { \"id\": \"5001\", \"type\": \"None\" },\n      { \"id\": \"5002\", \"type\": \"Glazed\" },\n      { \"id\": \"5005\", \"type\": \"Sugar\" },\n      { \"id\": \"5007\", \"type\": \"Powdered Sugar\" },\n      { \"id\": \"5006\", \"type\": \"Chocolate with Sprinkles\" },\n      { \"id\": \"5003\", \"type\": \"Chocolate\" },\n      { \"id\": \"5004\", \"type\": \"Maple\" }\n    ]\n}";
+		$scope.json = JSON.parse($scope.jsonString);
+		$scope.$watch('jsonString', function (newVal, oldVal) {
+			if (newVal !== oldVal) {
+				$scope.json = JSON.parse(newVal);
 			}
-		}
+		});
+		$scope.loadUrl = function () {
+			if ($scope.jsonUrl) {
+				$http.get($scope.jsonUrl).then(function (resp) {
+					$scope.jsonString = JSON.stringify(resp.data);
+					$scope.json = resp.data;
+				});
+			}
+		};
 	}]);
