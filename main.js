4cc631533fb0173820267e1a6203a01614f2f76b
Dan Miller <danmiller@bluedrop.com>
2016-08-22 23:55:06 -0230

    Use debounce to limit error message display.

--- main.js
+++ main.js
@@ -10,11 +10,13 @@ angular.module('jsonTreeDemo', ['angular-json-tree'])
 		$scope.json = {};
 		$scope.json.string = "{\n  \"id\": \"0001\",\n  \"type\": \"donut\",\n  \"name\": \"Cake\",\n  \"ppu\": 0.55,\n  \"batters\":\n    {\n      \"batter\":\n        [\n          { \"id\": \"1001\", \"type\": \"Regular\" },\n          { \"id\": \"1002\", \"type\": \"Chocolate\" },\n          { \"id\": \"1003\", \"type\": \"Blueberry\" },\n          { \"id\": \"1004\", \"type\": \"Devil's Food\" }\n        ]\n    },\n  \"topping\":\n    [\n      { \"id\": \"5001\", \"type\": \"None\" },\n      { \"id\": \"5002\", \"type\": \"Glazed\" },\n      { \"id\": \"5005\", \"type\": \"Sugar\" },\n      { \"id\": \"5007\", \"type\": \"Powdered Sugar\" },\n      { \"id\": \"5006\", \"type\": \"Chocolate with Sprinkles\" },\n      { \"id\": \"5003\", \"type\": \"Chocolate\" },\n      { \"id\": \"5004\", \"type\": \"Maple\" }\n    ]\n}";
 		$scope.json.object = JSON.parse($scope.json.string);
-		$scope.$watch('json.string', function (newVal, oldVal) {
-			if (newVal !== oldVal) {
+		var debounced;
+		$scope.handleError = function ($event, jsonString) {
+			$scope.error = null;
+			clearTimeout(debounced);
+			debounced = setTimeout(function() {
 				try {
-					$scope.error = null;
-					$scope.json.object = JSON.parse(newVal);
+					$scope.json.object = JSON.parse(jsonString);
 				} catch (e) {
 					console.log(e);
 					$scope.error = {
@@ -22,8 +24,9 @@ angular.module('jsonTreeDemo', ['angular-json-tree'])
 						show: true
 					}
 				}
-			}
-		});
+				$scope.$apply();
+			}, 1500);
+		};
 		$scope.loadUrl = function () {
 			if ($scope.json.url) {
 				$scope.json.badUrl = false;
